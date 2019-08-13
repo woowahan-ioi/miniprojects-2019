ed3587c729337c2dcb4abc9144e882d5dbcdcdce
MrKwon <erased11@gmail.com>
2019-08-13 20:42:24 +0900

    feat: 회원을 등록하는 기능 추가
    * SignUpRequestDto 회원 등록시 필요한 정보를 받아오기 위한 Dto

--- src/main/java/com/wootube/ioi/web/controller/UserController.java
+++ src/main/java/com/wootube/ioi/web/controller/UserController.java
@@ -1,12 +1,25 @@
 package com.wootube.ioi.web.controller;
 
+import com.wootube.ioi.service.UserService;
+import com.wootube.ioi.web.dto.SignUpRequestDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RequestMapping("/user")
 @Controller
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/signup")
     public String createUserFrom() {
@@ -16,5 +29,11 @@ public class UserController {
     @GetMapping("/login")
     public String createLoginFrom() {
         return "login";
+    }
+
+    @PostMapping("/signup")
+    public RedirectView signUp(SignUpRequestDto signUpRequestDto) {
+        userService.signUp(signUpRequestDto);
+        return new RedirectView("/user/login");
     }
 }
