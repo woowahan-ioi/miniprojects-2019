3c5875c9f4bb9127d5e3ce73030388fcbacc6f28
MrKwon <erased11@gmail.com>
2019-08-14 11:31:59 +0900

    test: findByEmail 메서드

---
+++ src/test/java/com/wootube/ioi/repository/UserRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.wootube.ioi.repository;
+
+import com.wootube.ioi.domain.User;
+import com.wootube.ioi.service.exception.NotFoundUserException;
+import com.wootube.ioi.web.dto.SignUpRequestDto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("이메일로 데이터 찾기")
+    @Test
+    void findByEmail() {
+        SignUpRequestDto signUpRequestDto = new SignUpRequestDto("루피", "luffy@luffy.com", "1234567a");
+        userRepository.save(signUpRequestDto.toUser());
+
+        User foundUser = userRepository.findByEmail(signUpRequestDto.getEmail())
+                .orElseThrow(NotFoundUserException::new);
+
+        assertThat(foundUser.getName()).isEqualTo(signUpRequestDto.getName());
+        assertThat(foundUser.getEmail()).isEqualTo(signUpRequestDto.getEmail());
+        assertThat(foundUser.getPassword()).isEqualTo(signUpRequestDto.getPassword());
+    }
+}
