2b033cc40889be6b83c281e8f872dd4c5ed626ce
yejilee <yjll1019@naver.com>
2019-08-16 16:28:06 +0900

    refactor: config 클래스명 변경

--- src/main/java/com/wootube/ioi/web/support/ModelConfig.java
+++ src/main/java/com/wootube/ioi/web/support/ModelConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Config {
+public class ModelConfig {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
