412faf0f4c0a4ba3d11a8faa3780ee215574774b
backlo <hjun4512@naver.com>
2019-08-18 16:54:31 +0900

    Merge remote-tracking branch 'upstream/develop' into feat-video-test
    # Conflicts:
    #	src/main/java/com/wootube/ioi/service/util/FileUploader.java
    #	src/main/java/com/wootube/ioi/web/support/AmazonS3Config.java
    #	src/test/java/com/wootube/ioi/domain/repository/UserRepositoryTest.java
    #	src/test/java/com/wootube/ioi/web/controller/CommentCommonControllerTest.java
    #	src/test/java/com/wootube/ioi/web/controller/HomeControllerTest.java
    #	src/test/java/com/wootube/ioi/web/controller/UserRestControllerTest.java

--- src/test/java/com/wootube/ioi/domain/repository/UserRepositoryTest.java
+++ src/test/java/com/wootube/ioi/domain/repository/UserRepositoryTest.java
@@ -13,10 +13,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-<<<<<<< HEAD
-//@ExtendWith(SpringExtension.class)
-=======
->>>>>>> upstream/develop
 public class UserRepositoryTest {
 
     @Autowired
