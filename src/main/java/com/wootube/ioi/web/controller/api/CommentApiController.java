7d01d1cc021596772896191f1be0e50782dfb81b
hyojaekim <alsghks2575@kmou.ac.kr>
2019-08-18 21:18:25 +0900

    refactor: Rest Controller 어노테이션 추가

--- src/main/java/com/wootube/ioi/web/controller/api/CommentApiController.java
+++ src/main/java/com/wootube/ioi/web/controller/api/CommentApiController.java
@@ -3,22 +3,14 @@ package com.wootube.ioi.web.controller.api;
 import com.wootube.ioi.service.CommentService;
 import com.wootube.ioi.service.dto.CommentRequestDto;
 import com.wootube.ioi.service.dto.CommentResponseDto;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
 @RequestMapping("/api/videos")
-@Controller
+@RestController
 public class CommentApiController {
     private final CommentService commentService;
 
@@ -26,13 +18,11 @@ public class CommentApiController {
         this.commentService = commentService;
     }
 
-    @ResponseBody
     @PostMapping("/{videoId}/comments")
     public ResponseEntity<CommentResponseDto> createComment(HttpSession session, @PathVariable Long videoId, @RequestBody CommentRequestDto commentRequestDto) {
         return new ResponseEntity<>(commentService.save(commentRequestDto), HttpStatus.CREATED);
     }
 
-    @ResponseBody
     @PutMapping("/{videoId}/comments/{commentId}")
     public ResponseEntity<Void> updateComment(@PathVariable Long videoId,
                                               @PathVariable Long commentId,
