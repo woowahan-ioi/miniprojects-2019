b2ac01b1a84cc1b3b8bb1c557ef52378b7e2d56d
mrkwon <erased11@gmail.com>
2019-08-20 18:03:03 +0900

    임시 브랜치 임시 커밋

---
+++ src/main/java/com/wootube/ioi/domain/model/VerifyKey.java
@@ -0,0 +1,26 @@
+package com.wootube.ioi.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class VerifyKey {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String verifyKey;
+}
--- src/main/java/com/wootube/ioi/service/UserService.java
+++ src/main/java/com/wootube/ioi/service/UserService.java
@@ -2,10 +2,12 @@ package com.wootube.ioi.service;
 
 import com.wootube.ioi.domain.exception.NotMatchPasswordException;
 import com.wootube.ioi.domain.model.User;
+import com.wootube.ioi.domain.model.VerifyKeyFactory;
 import com.wootube.ioi.domain.repository.UserRepository;
 import com.wootube.ioi.service.dto.EditUserRequestDto;
 import com.wootube.ioi.service.dto.LogInRequestDto;
 import com.wootube.ioi.service.dto.SignUpRequestDto;
+import com.wootube.ioi.service.exception.InActivatedUserException;
 import com.wootube.ioi.service.exception.LoginFailedException;
 import com.wootube.ioi.service.exception.NotFoundUserException;
 
@@ -19,11 +21,13 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final EmailService emailService;
 
     @Autowired
-    public UserService(UserRepository userRepository, ModelMapper modelMapper) {
+    public UserService(UserRepository userRepository, ModelMapper modelMapper, EmailService emailService) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.emailService = emailService;
     }
 
     public User createUser(SignUpRequestDto signUpRequestDto) {
@@ -32,9 +36,18 @@ public class UserService {
 
     public User readUser(LogInRequestDto logInRequestDto) {
         try {
-            return findByEmail(logInRequestDto.getEmail()).matchPassword(logInRequestDto.getPassword());
+            User savedEmail = findByEmail(logInRequestDto.getEmail());
+            checkInActive(savedEmail);
+            return savedEmail.matchPassword(logInRequestDto.getPassword());
         } catch (NotFoundUserException | NotMatchPasswordException e) {
             throw new LoginFailedException();
+        }
+    }
+
+    private void checkInActive(User savedEmail) {
+        if (!savedEmail.getIsActive()) {
+            emailService.sendMessage(savedEmail.getEmail());
+            throw new InActivatedUserException();
         }
     }
 
@@ -45,17 +58,17 @@ public class UserService {
 
     @Transactional
     public User updateUser(Long userId, EditUserRequestDto editUserRequestDto) {
-        return findById(userId).updateName(editUserRequestDto.getName());
+        return findByIdAndIsActiveTrue(userId).updateName(editUserRequestDto.getName());
     }
 
-    private User findById(Long userId) {
+    private User findByIdAndIsActiveTrue(Long userId) {
         return userRepository.findByIdAndIsActiveTrue(userId)
                 .orElseThrow(NotFoundUserException::new);
     }
 
     @Transactional
     public User deleteUser(Long userId) {
-        User deleteTargetUser = findById(userId);
+        User deleteTargetUser = findByIdAndIsActiveTrue(userId);
         deleteTargetUser.softDelete();
         return deleteTargetUser;
     }
