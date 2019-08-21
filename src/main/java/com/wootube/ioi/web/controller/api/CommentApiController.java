00439214fcc0d1ddaaeca3ba6fd81822ca07798c
hyojaekim <alsghks2575@kmou.ac.kr>
2019-08-21 15:00:49 +0900

    feat: 댓글 삭제 시 체크해야 하는 부분 추가

--- src/main/java/com/wootube/ioi/web/controller/api/CommentApiController.java
+++ src/main/java/com/wootube/ioi/web/controller/api/CommentApiController.java
@@ -3,17 +3,10 @@ package com.wootube.ioi.web.controller.api;
 import com.wootube.ioi.service.CommentService;
 import com.wootube.ioi.service.dto.CommentRequestDto;
 import com.wootube.ioi.service.dto.CommentResponseDto;
-
 import com.wootube.ioi.web.session.UserSession;
 import com.wootube.ioi.web.session.UserSessionManager;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -48,7 +41,8 @@ public class CommentApiController {
 
     @DeleteMapping("/{videoId}/comments/{commentId}")
     public ResponseEntity deleteComment(@PathVariable Long videoId, @PathVariable Long commentId) {
-        commentService.delete(commentId);
+        UserSession userSession = userSessionManager.getUserSession();
+        commentService.delete(commentId, userSession.getEmail(), videoId);
         return ResponseEntity.noContent()
                 .build();
     }
