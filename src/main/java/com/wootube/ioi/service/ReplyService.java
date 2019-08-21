86486d8b7a96f8e6586eeb308de913a02f719a39
hyojaekim <alsghks2575@kmou.ac.kr>
2019-08-21 17:27:14 +0900

    feat: ValidatorService 추가 및 적용
    * UserService
    * CommentService
    * VideoService

--- src/main/java/com/wootube/ioi/service/ReplyService.java
+++ src/main/java/com/wootube/ioi/service/ReplyService.java
@@ -2,6 +2,8 @@ package com.wootube.ioi.service;
 
 import com.wootube.ioi.domain.model.Comment;
 import com.wootube.ioi.domain.model.Reply;
+import com.wootube.ioi.domain.model.User;
+import com.wootube.ioi.domain.model.Video;
 import com.wootube.ioi.domain.repository.ReplyRepository;
 import com.wootube.ioi.service.dto.ReplyRequestDto;
 import com.wootube.ioi.service.dto.ReplyResponseDto;
@@ -12,17 +14,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReplyService {
-    private ReplyRepository replyRepository;
-    private CommentService commentService;
+    private final ReplyRepository replyRepository;
+    private final ValidatorService validatorService;
 
-    public ReplyService(ReplyRepository replyRepository, CommentService commentService) {
+    public ReplyService(ReplyRepository replyRepository, ValidatorService validatorService) {
         this.replyRepository = replyRepository;
-        this.commentService = commentService;
+        this.validatorService = validatorService;
     }
 
-    public ReplyResponseDto save(ReplyRequestDto replyRequestDto, Long commentId) {
-        Reply reply = new Reply(replyRequestDto.getContents(), commentService.findById(commentId));
-        Reply savedReply = replyRepository.save(reply);
+    public ReplyResponseDto save(ReplyRequestDto replyRequestDto, Long commentId, String email, Long videoId) {
+        User writer = validatorService.getUserService().findByEmail(email);
+        Video video = validatorService.getVideoService().findVideo(videoId);
+        Comment comment = validatorService.getCommentService().findById(commentId);
+
+        Reply savedReply = replyRepository.save(Reply.of(replyRequestDto.getContents(), comment, writer));
         return ReplyResponseDto.of(savedReply.getId(),
                 savedReply.getContents(),
                 savedReply.getUpdateTime());
@@ -32,7 +37,7 @@ public class ReplyService {
     public ReplyResponseDto update(ReplyRequestDto replyRequestDto, Long commentId, Long replyId) {
         //비디오 번호 확인하기
         //답글 작성자 확인하기
-        Comment comment = commentService.findById(commentId);
+        Comment comment = validatorService.getCommentService().findById(commentId);
         Reply reply = replyRepository.findById(replyId)
                 .orElseThrow(NotFoundReplyException::new);
         reply.update(comment, replyRequestDto.getContents());
@@ -44,7 +49,7 @@ public class ReplyService {
     public void delete(Long commentId, Long replyId) {
         //비디오 번호 확인하기
         //답글 작성자 확인하기
-        Comment comment = commentService.findById(commentId);
+        Comment comment = validatorService.getCommentService().findById(commentId);
         Reply reply = replyRepository.findById(replyId)
                 .orElseThrow(NotFoundReplyException::new);
 
