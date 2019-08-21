2e377d4f34dbff5bebd7b689d9081c809296ad1f
backlo <hjun4512@naver.com>
2019-08-21 18:56:39 +0900

    feat: Video Service 에 있는 삭제 메소드 변경

--- src/main/java/com/wootube/ioi/service/VideoService.java
+++ src/main/java/com/wootube/ioi/service/VideoService.java
@@ -1,17 +1,20 @@
 package com.wootube.ioi.service;
 
-import javax.transaction.Transactional;
-
+import com.wootube.ioi.domain.model.User;
 import com.wootube.ioi.domain.model.Video;
+import com.wootube.ioi.domain.repository.UserRepository;
 import com.wootube.ioi.domain.repository.VideoRepository;
 import com.wootube.ioi.service.dto.VideoRequestDto;
 import com.wootube.ioi.service.dto.VideoResponseDto;
+import com.wootube.ioi.service.exception.NotFoundUserException;
 import com.wootube.ioi.service.exception.NotFoundVideoException;
 import com.wootube.ioi.service.util.FileUploader;
 import org.modelmapper.ModelMapper;
-
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class VideoService {
@@ -19,22 +22,28 @@ public class VideoService {
     private final ModelMapper modelMapper;
 
     private final VideoRepository videoRepository;
+    private final UserRepository userRepository;
 
-    public VideoService(FileUploader fileUploader, ModelMapper modelMapper, VideoRepository videoRepository) {
+    public VideoService(FileUploader fileUploader, ModelMapper modelMapper, VideoRepository videoRepository, UserRepository userRepository) {
         this.fileUploader = fileUploader;
         this.modelMapper = modelMapper;
         this.videoRepository = videoRepository;
+        this.userRepository = userRepository;
     }
 
-    public VideoResponseDto create(MultipartFile uploadFile, VideoRequestDto videoRequestDto) {
+    public VideoResponseDto create(MultipartFile uploadFile, VideoRequestDto videoRequestDto, Long userId) {
         String videoUrl = fileUploader.uploadFile(uploadFile);
         String originFileName = uploadFile.getOriginalFilename();
-        videoRequestDto.setContentPath(videoUrl);
 
         Video video = modelMapper.map(videoRequestDto, Video.class);
-        video.setOriginFileName(originFileName);
+        User writer = findUser(userId);
 
+        video.initialize(videoUrl, originFileName, writer);
         return modelMapper.map(videoRepository.save(video), VideoResponseDto.class);
+    }
+
+    private User findUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow(NotFoundUserException::new);
     }
 
     public VideoResponseDto findById(Long id) {
@@ -52,16 +61,23 @@ public class VideoService {
         if (!uploadFile.isEmpty()) {
             fileUploader.deleteFile(video.getOriginFileName());
             String videoUrl = fileUploader.uploadFile(uploadFile);
-            video.setContentPath(videoUrl);
+            video.updateContentPath(videoUrl);
         }
         video.update(modelMapper.map(videoRequestDto, Video.class));
         videoRepository.save(video);
     }
 
     @Transactional
-    public void deleteById(Long id) {
-        Video video = findVideo(id);
+    public void deleteById(Long videoId) {
+        Video video = findVideo(videoId);
+//        Video writerVideo = video.matchWriter(userId);
+
         fileUploader.deleteFile(video.getOriginFileName());
-        videoRepository.deleteById(id);
+        videoRepository.deleteById(video.getId());
+    }
+
+    public List<Video> findVideosByWriter(Long writerId) {
+        User writer = findUser(writerId);
+        return videoRepository.findByWriter(writer);
     }
 }
