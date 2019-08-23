444dfc66e9c4535aaa8c31ccee7ff31864ee90bc
seokhyeonchoi <lkamna12@gmail.com>
2019-08-23 15:19:47 +0900

    refactor: template을 templates.js로 이동

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -78,40 +78,6 @@ const replyButton = (function () {
         function appendReply(reply, target) {
             const writtenTime = calculateWrittenTime(reply.updateTime);
 
-            const replyTemplate = `<li class="reply mrg-btm-30" data-commentid="${reply.id}">
-                            <img class="img-circle width-50 comment-writer-img" src="/images/default/eastjun_big.jpg" alt="">
-                            <div class="comment-block">
-                                <div class="font-size-13">
-                                    <span class="user-name">${reply.authorName}</span>
-                                    <span class="update-date">${writtenTime}</span>
-                                </div>
-                                <div class="comment-more-box">
-                                    <button class="comment-more-buttons reply-edit-button">
-                                        <i class="ti-pencil"> 수정</i>
-                                    </button>
-                                    <button class="comment-more-buttons reply-delete-button">
-                                        <i class="ti-trash"> 삭제</i>
-                                    </button>
-                                </div>
-                                <span class="reply-contents font-size-15">${reply.contents}</span>
-                                <div>
-                                    <button class="like-btn">
-                                        <i class="ti-thumb-up"></i>
-                                    </button>
-                                    <span>3.5천</span>
-                                </div>
-                            </div>
-                            <div class="comment-update-area display-none mrg-btm-50">
-                                <div>
-                                    <img class="img-circle width-50 comment-writer-img" src="/images/default/eastjun_big.jpg"
-                                         alt="">
-                                    <input class="comment-input" type="text" value="${reply.contents}">
-                                </div>
-                                <button class="btn comment-btn reply-update-cancel-btn">취소</button>
-                                <button class="btn comment-btn edit reply-update-btn">수정</button>
-                            </div>
-                        </li>`;
-
             const replyList = target.closest(".reply-area").querySelector(".reply-list");
 
             replyList.insertAdjacentHTML("beforeend", replyTemplate);
