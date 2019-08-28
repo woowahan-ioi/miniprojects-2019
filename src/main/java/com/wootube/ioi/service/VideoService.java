819dc3e5a61fac469346fce217d59dae0da00c7b
mrkwon <erased11@gmail.com>
2019-08-28 20:43:13 +0900

    tmp

--- src/main/java/com/wootube/ioi/service/VideoService.java
+++ src/main/java/com/wootube/ioi/service/VideoService.java
@@ -5,45 +5,61 @@ import com.wootube.ioi.domain.model.Video;
 import com.wootube.ioi.domain.repository.VideoRepository;
 import com.wootube.ioi.service.dto.VideoRequestDto;
 import com.wootube.ioi.service.dto.VideoResponseDto;
+import com.wootube.ioi.service.exception.FileConvertException;
 import com.wootube.ioi.service.exception.NotFoundVideoIdException;
 import com.wootube.ioi.service.exception.NotMatchUserIdException;
 import com.wootube.ioi.service.exception.UserAndWriterMisMatchException;
-import com.wootube.ioi.service.util.FileUploader;
 import com.wootube.ioi.service.util.UploadType;
+import com.wootube.ioi.service.util.bmoluffy.FileConverter;
+import com.wootube.ioi.service.util.bmoluffy.FileUploader2;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.transaction.Transactional;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
-import static java.util.stream.Collectors.*;
+import javax.transaction.Transactional;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class VideoService {
-    private final FileUploader fileUploader;
+    private final FileUploader2 fileUploader;
     private final ModelMapper modelMapper;
-
     private final VideoRepository videoRepository;
     private final UserService userService;
 
-    public VideoService(FileUploader fileUploader, ModelMapper modelMapper, VideoRepository videoRepository, UserService userService) {
+    public VideoService(FileUploader2 fileUploader, ModelMapper modelMapper, VideoRepository videoRepository, UserService userService) {
         this.fileUploader = fileUploader;
         this.modelMapper = modelMapper;
         this.videoRepository = videoRepository;
         this.userService = userService;
     }
 
-    public VideoResponseDto create(MultipartFile uploadFile, VideoRequestDto videoRequestDto) {
-        List<String> urls = fileUploader.uploadFile(uploadFile, UploadType.VIDEO, UploadType.THUMBNAIL);
+    public VideoResponseDto create(MultipartFile uploadFile, VideoRequestDto videoRequestDto) throws IOException {
+        File convertedVideo = FileConverter.convert(uploadFile)
+                .orElseThrow(FileConvertException::new);
+
+        String videoUrl = fileUploader.uploadFile(convertedVideo, UploadType.VIDEO);
+
+        File convertedThumbnail = FileConverter.convert(convertedVideo)
+                .orElseThrow(FileConvertException::new);
+
+        String thumbnailUrl = fileUploader.uploadFile(convertedThumbnail, UploadType.THUMBNAIL);
+
         String originFileName = uploadFile.getOriginalFilename();
+        String thumbnailFileName = convertedThumbnail.getName();
+
         User writer = userService.findByIdAndIsActiveTrue(videoRequestDto.getWriterId());
 
         Video video = modelMapper.map(videoRequestDto, Video.class);
-        video.initialize(urls, originFileName, writer);
+        video.initialize(videoUrl, thumbnailUrl, originFileName, thumbnailFileName, writer);
         return modelMapper.map(videoRepository.save(video), VideoResponseDto.class);
     }
 
@@ -64,14 +80,25 @@ public class VideoService {
     }
 
     @Transactional
-    public void update(Long id, MultipartFile uploadFile, VideoRequestDto videoRequestDto) {
+    public void update(Long id, MultipartFile uploadFile, VideoRequestDto videoRequestDto) throws IOException {
         Video video = findById(id);
         matchWriter(videoRequestDto.getWriterId(), id);
 
         if (!uploadFile.isEmpty()) {
-            fileUploader.deleteFile(video.getOriginFileName(), UploadType.VIDEO, UploadType.THUMBNAIL);
-            List<String> videoUrl = fileUploader.uploadFile(uploadFile, UploadType.VIDEO, UploadType.VIDEO);
-            video.updateContentPath(videoUrl);
+            fileUploader.deleteFile(video.getOriginFileName(), UploadType.VIDEO);
+            fileUploader.deleteFile(video.getThumbnailFileName(), UploadType.THUMBNAIL);
+
+            File convertedVideo = FileConverter.convert(uploadFile)
+                    .orElseThrow(FileConvertException::new);
+
+            String videoUrl = fileUploader.uploadFile(convertedVideo, UploadType.VIDEO);
+
+            File convertedThumbnail = FileConverter.convert(convertedVideo)
+                    .orElseThrow(FileConvertException::new);
+
+            String thumbnailUrl = fileUploader.uploadFile(convertedThumbnail, UploadType.THUMBNAIL);
+
+            video.updateContentPath(videoUrl, thumbnailUrl);
         }
         video.update(modelMapper.map(videoRequestDto, Video.class));
         videoRepository.save(video);
@@ -83,7 +110,9 @@ public class VideoService {
         if (!video.matchWriter(userId)) {
             throw new UserAndWriterMisMatchException();
         }
-        fileUploader.deleteFile(video.getOriginFileName(), UploadType.VIDEO, UploadType.THUMBNAIL);
+        fileUploader.deleteFile(video.getOriginFileName(), UploadType.VIDEO);
+        fileUploader.deleteFile(video.getThumbnailFileName(), UploadType.THUMBNAIL);
+
         videoRepository.deleteById(video.getId());
     }
 
