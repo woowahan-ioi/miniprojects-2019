4aaba7863c874ee70afabed74465e076aac5b496
seokhyeonchoi <lkamna12@gmail.com>
2019-08-29 09:43:57 +0900

    refactor: FileUploader와 FileConverter 분리

--- src/main/java/com/wootube/ioi/domain/model/Video.java
+++ src/main/java/com/wootube/ioi/domain/model/Video.java
@@ -3,9 +3,11 @@ package com.wootube.ioi.domain.model;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+
 import java.util.List;
 
 @Entity
@@ -36,6 +38,10 @@ public class Video extends BaseEntity {
     @Column(nullable = false)
     private String originFileName;
 
+    @Lob
+    @Column(nullable = false)
+    private String thumbnailFileName;
+
     @Column(columnDefinition = "long default 0")
     private long views;
 
@@ -49,22 +55,30 @@ public class Video extends BaseEntity {
     }
 
     public void update(Video updateVideo) {
-        if (updateVideo.contentPath != null) {
-            this.contentPath = updateVideo.contentPath;
-        }
         this.title = updateVideo.title;
         this.description = updateVideo.description;
     }
 
-    public void updateContentPath(List<String> urls) {
-        this.contentPath = urls.get(CONTENT_PATH);
-        this.thumbnailPath = urls.get(THUMBNAIL_PATH);
+    public void updateTitle(String title) {
+        this.title = title;
     }
 
-    public void initialize(List<String> urls, String originFileName, User writer) {
-        this.contentPath = urls.get(CONTENT_PATH);
-        this.thumbnailPath = urls.get(THUMBNAIL_PATH);
+    public void updateDescription(String description) {
+        this.description = description;
+    }
+
+    public void updateVideo(String contentPath, String originFileName, String thumbnailPath, String thumbnailFileName) {
+        this.contentPath = contentPath;
         this.originFileName = originFileName;
+        this.thumbnailPath = thumbnailPath;
+        this.thumbnailFileName = thumbnailFileName;
+    }
+
+    public void initialize(String contentPath, String thumbnailPath, String originFileName, String thumbnailFileName, User writer) {
+        this.contentPath = contentPath;
+        this.thumbnailPath = thumbnailPath;
+        this.originFileName = originFileName;
+        this.thumbnailFileName = thumbnailFileName;
         this.writer = writer;
     }
 
@@ -75,6 +89,7 @@ public class Video extends BaseEntity {
     public void increaseViews() {
         this.views++;
     }
+
 
 //    public void verifyOriginFileName(String originFileName) {
 //        originFileName.mat
--- src/main/java/com/wootube/ioi/domain/repository/VideoRepository.java
+++ src/main/java/com/wootube/ioi/domain/repository/VideoRepository.java
@@ -4,7 +4,6 @@ import com.wootube.ioi.domain.model.User;
 import com.wootube.ioi.domain.model.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sun.jvm.hotspot.debugger.Page;
 
 import java.util.List;
 
