01b16117ca82a9f2f9614d88eeb4551e08c3f158
backlo <hjun4512@naver.com>
2019-09-01 17:13:00 +0900

    fix: video-edit progress bar 삭제

--- src/main/resources/static/js/video-edit.js
+++ src/main/resources/static/js/video-edit.js
@@ -6,22 +6,6 @@ function unHover(element) {
     element.setAttribute('src', '/images/logo/youtube-upload-logo.png');
 }
 
-function move() {
-    const elem = document.getElementById("progressBar");
-    const width = 0;
-    const id = setInterval(frame, 0);
-
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-        } else {
-            width++;
-            elem.style.width = width + '%';
-            elem.innerHTML = width + '%';
-        }
-    }
-}
-
 function videoSubmit(event) {
     const submitButton = event.target;
     document.querySelector("#video-form").submit();
