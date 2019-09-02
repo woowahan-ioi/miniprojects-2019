acc018235adac6355d7998d6181214a105ab4635
backlo <hjun4512@naver.com>
2019-09-02 13:32:06 +0900

    fix: video description, title 글자수 제한 기능 구현

--- src/main/resources/static/js/video-edit.js
+++ src/main/resources/static/js/video-edit.js
@@ -8,8 +8,22 @@ function unHover(element) {
 
 function videoSubmit(event) {
     const submitButton = event.target;
+    if(checkTextLength() === false) {
+        return;
+    }
     document.querySelector("#video-form").submit();
     submitButton.disabled = true;
 }
 
+function checkTextLength() {
+    const textArea = document.querySelector("#description");
+    if(textArea.value.length > 1000) {
+        alert("1000자 이내의 내용을 입력해주세요.");
+        textArea.value = textArea.value.substr(0, 1000);
+        return false;
+    }
+    return true;
+}
+
+document.querySelector("#description").addEventListener("keyup", checkTextLength);
 document.querySelector("#video-submit-button").addEventListener("click", videoSubmit);
