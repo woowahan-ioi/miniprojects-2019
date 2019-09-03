cdfbbf8390143094f06c058499932525e8d96f39
mrkwon <erased11@gmail.com>
2019-09-03 14:04:48 +0900

    feat: Timezone 설정

--- src/main/java/com/wootube/ioi/WootubeApplication.java
+++ src/main/java/com/wootube/ioi/WootubeApplication.java
@@ -4,9 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @PropertySource("classpath:email.properties")
 public class WootubeApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WootubeApplication.class, args);
     }
